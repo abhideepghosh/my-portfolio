ce572055cba6bdcf1f1da0ba71c153b4bceed62a
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-12-03 16:32:50 +0530

    Portfolio Completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import './footer.css';
-
+import React from 'react'
+import './footer.css'
+import {FaCode} from 'react-icons/fa'
+import {FiFacebook} from 'react-icons/fi'
+import {BsInstagram} from'react-icons/bs'
+import {FiTwitter} from 'react-icons/fi'
 const Footer = () => {
-	return(
+  return (
+    <footer id='footer'>
+      <a href="#" className='footer__logo'><FaCode/></a>
 
-		<div>Footer</div>
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contacts</a></li>
+      </ul>
 
-		);
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/abhideeprocks/"><FiFacebook/></a>
+        <a href="https://www.instagram.com/mrblacknightsky/"><BsInstagram/></a>
+        <a href="https://twitter.com/gabhiaf98"><FiTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Abhideep Ghosh</small>
+      </div>
+
+
+    </footer>
+  )
 }
 
 export default Footer;
