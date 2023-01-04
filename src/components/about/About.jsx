80d2a3badba569d579dc417da8c5ccac7e4cfde8
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-01-04 17:27:43 +0530

    Updates on portfolio

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,50 +1,64 @@
-import React from 'react';
-import './about.css';
-import ME from '../../assets/me-about.jpg'
-import {FaAward} from 'react-icons/fa'
-import {GiTrophyCup} from 'react-icons/gi';
-import {VscFolderLibrary} from 'react-icons/vsc'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { GiTrophyCup } from "react-icons/gi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-	return (
+  return (
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            {/* <img src={ME} alt="About me" /> */}
+            <img
+              src="https://miro.medium.com/max/900/1*q99J7XLYVWgOt69E8IuXqw.gif"
+              alt="About me"
+            />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>Fresher</small>
+            </article>
 
-		<section id="about">
-			<h5>Get to know</h5>
-		    <h2>About me</h2>
-		    <div className="container about__container">
-		        <div className="about__me">
-		          <div className="about__me-image">
-		            <img src={ME} alt="About me" />
-		          </div>
-		        </div>
-		        <div className="about__content">
-		          <div className="about__cards">
-		            <article className="about__card">
-		              <FaAward className='about__icon'/>
-		              <h5>Experience</h5>
-		              <small>Fresher</small>
-		            </article>
+            <article className="about__card">
+              <GiTrophyCup className="about__icon" />
+              <h5>Top Performer</h5>
+              <small>Consistently Ranked 1 In Newton Leaderboard</small>
+            </article>
 
-		            <article className="about__card">
-		              <GiTrophyCup className='about__icon'/>
-		              <h5>Top Performer</h5>
-		              <small>Consistently Ranked 1 In Newton Leaderboard</small>
-		            </article>
-
-		            <article className="about__card">
-		              <VscFolderLibrary className='about__icon'/>
-		              <h5>Projects</h5>
-		              <small>5+ Completed</small>
-		            </article>
-		          </div>
-		          <p>I am Abhideep Ghosh, a Full Stack Web Developer. The languages I love to code in are HTML, CSS, JS and Java. I've worked on many React projects and lead few team projects in Newton. I've also taught students while being one of the leaders of Super 50 group in Newton.</p>
-		          <a href="#contact" className='btn btn-primary'>Let's talk</a>
-
-		        </div>
-	      	</div>
-		</section>
-
-		);
-}
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>5+ Completed</small>
+            </article>
+          </div>
+          <p>
+            I am Abhideep Ghosh, a Full Stack Web Developer. My passion lies in
+            solving complex problems with tailored data and algorithms and
+            creating stunning websites. I have been obsessed with the idea of
+            using software to solve practical problems. Software engineering is
+            a never-ending puzzle that I am passionately engaged in solving. I
+            believe in the power of programming to transform and improve the
+            lives of people around the world. I really enjoy learning new things
+            and connecting with people across a range of industries, so don't
+            hesitate to reach out if you'd like to get in touch. I've worked on
+            many React projects and led team projects in Newton.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default About;
