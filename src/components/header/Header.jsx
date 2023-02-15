a0b295fd99a5fc636a888aba01e663fb6b35ab5a
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-02-15 04:42:05 +0000

    V1.1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-// import ME from "../../assets/me.png";
+import ME from "../../assets/me.gif";
 import HeaderSocial from "./HeaderSocial";
 
 const Header = () => {
@@ -15,11 +15,7 @@ const Header = () => {
         <HeaderSocial />
 
         <div className="me">
-          {/* <img src={ME} alt="me" /> */}
-          <img
-            src="https://camo.githubusercontent.com/97d1828fe16ccca3417229fc085cfc96062bd74c8787f80131ddc9462ce4ed51/68747470733a2f2f736f6669616e6568616d6c616f75692e6769746875622e696f2f6a756e6b2f6c6f636b646f6f722f6c6f676f732f6c6f676f323035783235302e676966?raw=true"
-            alt="me"
-          />
+          <img src={ME} alt="me" />
         </div>
 
         <a href="#contact" className="scroll__down">
